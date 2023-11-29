6c2303b853e67cb262774d45e14b2a578e956d39
trantrunghieu <trantrunghieu@vnpt.vn>
2023-11-29 13:41:49 +0700

    init commit

---
+++ src/main/java/hieutt/cicddemo/CICDDemoApplication.java
@@ -0,0 +1,13 @@
+package hieutt.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CICDDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CICDDemoApplication.class, args);
+	}
+
+}
