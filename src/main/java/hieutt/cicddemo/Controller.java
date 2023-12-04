fe714c9be5b02de8046f4e00991412540d90e467
Hieu Tran <85477665+hieutran249@users.noreply.github.com>
2023-12-04 15:24:00 +0700

    Update Controller.java

--- src/main/java/hieutt/cicddemo/Controller.java
+++ src/main/java/hieutt/cicddemo/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping()
     public String home() {
-        return "hello";
+        return "hello 2";
     }
 }
